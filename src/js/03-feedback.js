1d9d0d115035dc722f807f324e2928852f5e2851
Senderion13 <serovandrey2004@gmail.com>
2022-08-19 14:52:24 +0300

    3

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,21 +1,22 @@
 import _ from 'lodash';
 
 let form = document.querySelector(".feedback-form");
-let email = document.querySelector('input');
-let message = document.querySelector('textarea');
+let formElements = form.elements;
 
-let formInput = (e) => {
-    localStorage.setItem("feedback-form-state", JSON.stringify({
-        "email": `${email.value}`,
-        "message": `${message.value}`
-    }));
+let formInput = () => {
+    let temp = {};
+    for (let i = 0; i < formElements.length - 1; i++) {
+        temp[formElements[i].name] = formElements[i].value;
+    }
+    localStorage.setItem("feedback-form-state", JSON.stringify(temp));
 }
 form.addEventListener("input", _.throttle(formInput, 500));
 
 document.addEventListener("DOMContentLoaded", () => {
     if(localStorage.getItem("feedback-form-state") != null){
-        email.value = JSON.parse(localStorage.getItem("feedback-form-state")).email;
-        message.value = JSON.parse(localStorage.getItem("feedback-form-state")).message;
+        for (let i = 0; i < formElements.length - 1; i++) {
+            formElements[i].value = JSON.parse(localStorage.getItem("feedback-form-state"))[formElements[i].name];
+        }
     }
 })
 
